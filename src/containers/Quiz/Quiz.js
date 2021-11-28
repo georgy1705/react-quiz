324a1f6e95c2174b76d5ad27418ebdb47be7639c
Georgy Krasnov <shimgk1705@gmail.com>
2021-11-29 02:15:33 +0300

    Components of buttons are made. A side menu has been created.

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,17 +1,123 @@
 import React, { Component } from 'react';
 import classes from './Quiz.css'
 
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz';
+
 
 class Quiz extends Component {
-
     state = {
-        quiz: [],
+        results: {},
+        isFinished: false,
+        activeQuestion: 0,
+        answerState: null,
+        quiz: [
+            {
+                question: 'Какого цвета небо?',
+                rightAnswerId: 2,
+                id: 1,
+                answers: [
+                    {text: 'Черный', id: 1},
+                    {text: 'Негр', id: 2},
+                    {text: 'Белый', id: 3},
+                    {text: 'Голубой', id: 4},
+                ]
+            },
+            {
+                question: 'В каком году основали кабана?',
+                rightAnswerId: 3,
+                id: 2,
+                answers: [
+                    {text: '1700', id: 1},
+                    {text: '1705', id: 2},
+                    {text: '1703', id: 3},
+                    {text: '1803', id: 4},
+                ]
+            }
+        ],
+    }
+
+    onAnswerClickHandler = answerId => {
+        if (this.state.answerState) {
+            const key = Object.keys(this.state.answerState)[0]
+            if (this.state.answerState[key] === 'success') {
+                return
+            }
+        }
+
+        const question = this.state.quiz[this.state.activeQuestion]
+        const results = this.state.results
+
+        if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) {
+                results[question.id] = 'success'
+            }
+            this.setState({
+                answerState: {[answerId]: 'success'},
+                results
+            })
+
+            const timeout = window.setTimeout(() => {
+                if (this.isQuizFinished()) {
+                    this.setState({
+                        isFinished: true
+                    })
+                } else {
+                    this.setState({
+                    activeQuestion: this.state.activeQuestion + 1,
+                    answerState: null
+                    }) 
+                }
+
+                window.clearTimeout(timeout)
+            }, 1150)
+
+            
+        } else {
+            results[question.id] = 'error'
+            this.setState({
+                answerState: {[answerId]: 'error'},
+                results
+            })
+        }
+
+        
+    }
+    
+    isQuizFinished = () => this.state.activeQuestion + 1 === this.state.quiz.length
+
+    retryHandler = () => {
+        this.setState({
+            activeQuestion: 0,
+            answerState: null,
+            isFinished: false,
+            results: {}
+        })
     }
 
     render() {
         return(
             <div className={classes.Quiz}>
-                <h1>Quiz kaban</h1>
+                <div className={classes.QuizWrapper}>
+                    <h1>Ответьте на все вопросы</h1>
+
+                    {
+                        this.state.isFinished ?
+                        <FinishedQuiz 
+                            results={this.state.results}
+                            quiz={this.state.quiz}
+                            onRetry={this.retryHandler}
+                        /> :
+                        <ActiveQuiz 
+                        question={this.state.quiz[this.state.activeQuestion].question}
+                        answers={this.state.quiz[this.state.activeQuestion].answers}
+                        onAnswerClick={this.onAnswerClickHandler}
+                        quizLength={this.state.quiz.length}
+                        answerNumber={this.state.activeQuestion + 1}
+                        state={this.state.answerState}
+                    />
+                    }
+                </div>
             </div>
         )
     }
